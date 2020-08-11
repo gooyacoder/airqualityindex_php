2e0e1fc2dabd56de49920ae0b7524e91af56a51d
Amirhosein Mahdiyar <amirhosein@localhost.localdomain>
2020-08-11 13:01:09 +0430

    Website construction began

---
+++ includes/header.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <link href='http://www.fontonline.ir/css/BZar.css' rel='stylesheet' type='text/css'>
+    <link href='http://www.fontonline.ir/css/BHoma.css' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,300&display=swap" rel="stylesheet"> 
+    <link href='http://www.fontonline.ir/css/IranNastaliq.css' rel='stylesheet' type='text/css'>
+    <link href='http://www.fontonline.ir/css/BTitrBold.css' rel='stylesheet' type='text/css'>
+    <link href='http://www.fontonline.ir/css/BSinaBold.css' rel='stylesheet' type='text/css'>
+    <link href="css/Style_001.css" rel='stylesheet' type='text/css'>
+
+    <title>Air Quality Search powered by Pooyesh Afzar</title>
+  </head>
+  <body>
+
+    <div class="links site_top">
+      <h1 class="text-center" id="page_title">نمایش شاخص کیفیت هوای شهرهای جهان</h1>
+      <img src="img/logo.jpg" alt="logo" width="70px" height="70px" />
+    </div>
+    <nav class="navbar navbar-xs navbar-expand-lg navbar-light links">
+      <a class="btn btn-xs btn-outline-light about_btn" href="">خانه</a>
+      <a class="btn btn-xs btn-outline-light about_btn" href="">درباره ما <span class="sr-only">(current)</span></a>
+      <button class="navbar-toggler" id="btn_collapse" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+           
+          </li>
+          
+        </ul>
+        <form class="form-inline my-2 my-lg-0" method="POST" action="api.php">
+          <input class="form-control mr-sm-2" id="city_input" type="search" placeholder="نام شهر را وارد کنید" aria-label="Search" name="city">
+          <button class="btn btn-xs btn-outline-light my-2 my-sm-0 search_btn" type="submit">جستجو</button>
+        </form>
+      </div>
+    </nav>
+
+    <div class="container">
--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php 
+	require_once 'includes/header.php';
+?>
 
-	$city = 'shiraz';
 
-	$url = 'http://api.waqi.info/feed/' . $city . '/?token=e6089546a3dc14e1fe3d717777e9c7df3e7e5f99';
 
-	$json = file_get_contents($url);
 
-	$result = json_decode($json, true);
 
-	echo "<h1>" . $result['data']['aqi'] . "<h1>";
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+
+	require_once 'includes/footer.php';
 
 ?>
