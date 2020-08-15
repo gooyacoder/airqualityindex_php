be51491caf543af0e73d1dd6c6ef9d777ba081bb
Amirhosein Mahdiyar <amirhosein@localhost.localdomain>
2020-08-15 15:28:20 +0430

    meta tags for seo added

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,9 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="title" content="شاخص کیفیت هوای شهرهای جهان">
+    <meta name="description" content="شاخص کیفیت هوا">
+    <meta name="keywords" content="شاخص کیفیت هوا,air quality index">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
