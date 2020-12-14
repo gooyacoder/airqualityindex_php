240b4a7e2910132e218052a18c93c254e6f6864a
amirhosein <gooyacoder@gmail.com>
2020-12-14 12:07:13 +0330

    24_9_1399 Cron Jobs for storing aqi of Morning, Noon and Afternoon created

---
+++ scripts/afternoon.php
@@ -0,0 +1,31 @@
+<?php
+	$servername = "localhost";
+	$username = "airquali_admin";
+	$password = "P3wi9uiRwBYSTag";
+	$dbname = "airquali_data_db";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	$city = 'shiraz';
+	$url = 'http://api.waqi.info/feed/' . $city . '/?token=e6089546a3dc14e1fe3d717777e9c7df3e7e5f99';
+	$json = file_get_contents($url);
+	$result = json_decode($json, true);
+	$aqi = $result['data']['aqi'];
+	$time_of_day = "Afternoon";
+	$aqi_date = date('Y-m-d');
+
+	$sql = "INSERT INTO shiraz_aqi_table (aqi, time_of_day, aqi_date) VALUES ($aqi, '$time_of_day', '$aqi_date')";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$conn->close();
+?> 
