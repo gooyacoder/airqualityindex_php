328faf1f09875e9612d9109309d55f69eceab696
Amirhosein Mahdiyar <36479394+gooyacoder@users.noreply.github.com>
2021-03-29 17:41:30 +0430

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -2,28 +2,21 @@
 <html lang="en">
 
 <head>
+    
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="title" content="شاخص کیفیت هوای شهرهای جهان">
     <meta name="description" content="شاخص کیفیت هوا">
     <meta name="keywords" content="شاخص کیفیت هوا,air quality index">
+    
     <!-- Bootstrap CSS -->
-
-<<<<<<< HEAD
     <link href="css/bootstrap.min.css" rel='stylesheet' type='text/css'>
     <link href="css/Style_001.css" rel='stylesheet' type='text/css'>
     <link href="css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-
-=======
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
-    <link href="css/bootstrap.min.css" rel='stylesheet' type='text/css'>
-    <link href="css/Style_001.css" rel='stylesheet' type='text/css'>
     
->>>>>>> 6543975c64e2e2c945f4e21102cb67349dd54efb
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    
     <title>Nickweb Air Quality</title>
 </head>
 
